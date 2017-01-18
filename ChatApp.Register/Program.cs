edab65bc815ad9efa825024fc373919f13c078be
Michał Sobański <Mikiawm@gmail.com>
2017-01-18 21:09:38 +0100

    Peer to peer

---
+++ ChatApp.Register/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.PeerToPeer;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Cloud c in Cloud.GetAvailableClouds())
+            {
+                Console.WriteLine("Cloud {0}", c.Name);
+            }
+            Console.WriteLine();
+            Console.Write("Enter Peer Name Classifier: ");
+
+            string classifier = Console.ReadLine();
+
+            PeerName peerName = new PeerName(classifier, PeerNameType.Unsecured);
+
+            using (PeerNameRegistration registration = new PeerNameRegistration(peerName, 8080))
+            {
+                string timestamp = string.Format("Peer Created at: {0}", DateTime.Now.ToShortDateString());
+                registration.Comment = timestamp;
+
+                UnicodeEncoding encoder = new UnicodeEncoding();
+                byte[] data = encoder.GetBytes(timestamp);
+                registration.Data = data;
+
+                try
+                {
+                    registration.Start();
+                    Console.WriteLine("Peer Registration Successful.");
+                    Console.WriteLine("Peer Host Name: {0}", registration.PeerName.PeerHostName);
+                    Console.WriteLine("Press enter to exit");
+
+                    Console.ReadLine();
+                    registration.Stop();
+                }
+                catch(PeerToPeerException ex)
+                {
+                    Console.WriteLine("PeerToPeer Exception: {0}", ex);
+                }
+            }   
+        }
+    }
+}
--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.PeerToPeer;
 
 namespace ChatApp.Ui
 {
     class Program
     {
+        static PeerNameRecordCollection records;
+
         static void Main(string[] args)
         {
+            Console.Write("Peer Classifer to Resolve: ");
+            string classifier = Console.ReadLine();
+
+            PeerNameResolver resolver = new PeerNameResolver();
+            
+            try
+            {
+                records = resolver.Resolve(new PeerName(classifier, PeerNameType.Unsecured));
+                DisplayResults();
+            }
+            catch(PeerToPeerException ex)
+            {
+                Console.WriteLine("PeerToPeer Exception: {0}", ex.Message);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static void DisplayResults()
+        {
+           if(records.Any())
+            {
+                UnicodeEncoding encoder = new UnicodeEncoding();
+                Console.WriteLine();
+
+                foreach(var record in records)
+                {
+                    string data = record.Data != null ? encoder.GetString(record.Data) : string.Empty;
+
+                    Console.WriteLine("***Peer {0}\n\r\tHost: {1}\n\r\tComment: {2}\n\r\tData: {3}", record.PeerName.ToString(), record.PeerName.PeerHostName, record.Comment, data);
+                    foreach( var endpoint in record.EndPointCollection)
+                    {
+                        Console.WriteLine("\tEndpoint{0}, Port{1}", endpoint.Address, endpoint.Port);
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
